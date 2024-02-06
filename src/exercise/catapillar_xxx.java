adcdd951609ce6539538b0283fd4c3e6b5d128ad
admin <admin@LAPTOP-FJ3D388H>
2024-02-06 19:41:21 +0530

    commit

--- src/exercise/catapillar_xxx.java
+++ src/exercise/catapillar_xxx.java
@@ -1,5 +1,13 @@
 package exercise;
 
 public class catapillar_xxx {
-
+	int moves,cabbages;
+	
+	 catapillar_xxx(int moves, int cabbages){
+		 this.moves=moves;
+		 this.cabbages=cabbages;
+	 }
+	 int get_moves() {
+		 return moves;
+	 }
 }
